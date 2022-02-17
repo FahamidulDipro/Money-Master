586cc8a62555e9197cb33a314d501e5774ca2c25
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-17 13:12:12 +0600

    Resetting all input values to 0 after refreshing page

--- main.js
+++ main.js
@@ -61,10 +61,24 @@ function positiveOnly(name, num) {
     if (!isNaN(num) && num >= 0) {
         document.getElementById(name + '-err').classList.add('hidden');
         document.getElementById(name + '-err').classList.remove('block');
+        document.getElementById('total-expenses').classList.remove('hidden');
+        document.getElementById('balance').classList.remove('hidden');
         return num;
 
     } else {
         document.getElementById(name + '-err').classList.add('block');
         document.getElementById(name + '-err').classList.remove('hidden');
+        document.getElementById('total-expenses').classList.add('hidden');
+        document.getElementById('balance').classList.add('hidden');
     }
+}
+
+
+//Refreshing will set 0
+window.onload = function() {
+    document.getElementById('income').value = '0';
+    document.getElementById('food').value = '0';
+    document.getElementById('rent').value = '0';
+    document.getElementById('cloths').value = '0';
+    document.getElementById('saving-input').value = '0';
 }
